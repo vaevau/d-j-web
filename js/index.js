bfc4a25f68844471f8be0e6555b90327e0642bc3
ArivnC <ArivnC@qq.com>
2021-01-03 23:30:16 +0800

    导航列表

---
+++ js/index.js
@@ -0,0 +1,75 @@
+;(function(doc){
+    var arr = ['您好，请登录','我的订单','我的京东','京东会员','企业采购','客户服务','网站导航','手机京东'];
+    var oCurntPlace = doc.querySelector('.J_current_place');    
+    var oRegionalLs = doc.querySelector('#J_nav_ls .regional-ls');
+    var aNavLk = doc.getElementsByClassName('J_nav_lk');
+    var aNavLs = doc.getElementsByClassName('J_nav_ls');    
+    var i = 0, aLkLen = aNavLk.length,
+        j = 0, aLsLen = aNavLs.length;
+    var h = s = 0;  //记录鼠标是否在目标身上 
+
+    var init = function(){
+        bindEvent();
+    }
+    function bindEvent(){  
+        oCurntPlace.addEventListener('mouseover', show, false);
+        oCurntPlace.addEventListener('mouseout', hide, false); 
+        oRegionalLs.addEventListener('mouseover', show, false);
+        oRegionalLs.addEventListener('mouseout', hide, false); 
+        for(; i<aLkLen; i++){ 
+            aNavLk[i].idx = i;        
+            aNavLk[i].addEventListener('mouseover', showNavList, false);
+            aNavLk[i].addEventListener('mouseout', hideNavList, false);
+        }
+        for(; j<aLsLen; j++){ 
+            aNavLs[j].idx = j;        
+            aNavLs[j].addEventListener('mouseover', showNavList, false);
+            aNavLs[j].addEventListener('mouseout', hideNavList, false);
+        }
+    }
+    function addClass(obj, cn){
+        if(!hasClass(obj, cn)){
+            obj.className += ' ' + cn;
+        }
+     }
+     function hasClass(obj, cn){
+        var reg = new RegExp("\\b" + cn + "\\b");
+        return reg.test(obj.className);
+     }
+     function rmClass(obj, cn){
+        var reg = new RegExp("\\b" + cn + "\\b");
+        return obj.className = obj.className.replace(reg,'');  
+     }
+   
+    function show(){   
+        h = s = 1;                     
+        addClass(oRegionalLs,'show');
+    }
+    function hide(){
+        h = s = 0;
+        setTimeout(function(){
+            if(h !==1 || s !== 1){
+                rmClass(oRegionalLs, 'show');
+            }
+        },100);
+    }
+
+     function showNavList(){
+         var index = this.idx;
+         h = s = 1; 
+        addClass(aNavLs[index], 'show');            
+     }
+     function hideNavList(){
+        var index = this.idx;
+         h = s = 0;         
+        if(h !==1 || s !== 1){
+            rmClass(aNavLs[index], 'show'); 
+            }                  
+     }
+
+    init();
+})(document);
+
+
+
+
