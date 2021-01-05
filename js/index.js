6b67f74e30ed4899cafde1102a8876d1d4f720e2
ArivnC <ArivnC@qq.com>
2021-01-05 12:53:22 +0800

    导航js样式改善

--- js/index.js
+++ js/index.js
@@ -5,7 +5,9 @@
     var aNavLk = doc.getElementsByClassName('J_nav_lk');
     var aNavLs = doc.getElementsByClassName('J_nav_ls');
     var aCateLk = doc.querySelectorAll('.J_cate_lk li');
-    var aCateLs = doc.getElementsByClassName('cate-item'); 
+    var aCateLs = doc.getElementsByClassName('cate-item');
+    // var oul = doc.getElementById('J_cate_lk');
+    
     var i = j = k = l = 0, aLkLen = aNavLk.length,
                            aLsLen = aNavLs.length,
                            cateLkLen = aCateLk.length,
@@ -36,25 +38,54 @@
             aCateLk[k].addEventListener('mouseover', showCateList, false);
             aCateLk[k].addEventListener('mouseout', hideCateList, false);
         }
+        //事件委托
+        // oul.addEventListener('mouseover', sEvtEntruse, false);
+        // oul.addEventListener('mouseout', HEvtEntruse, false);
         for (; l<cateLsLen; l++){
-            aCateLs[l].idx = l;                  
+            aCateLs[l].idx = l;            
             aCateLs[l].addEventListener('mouseover', showCateList, false);
             aCateLs[l].addEventListener('mouseout', hideCateList, false);
         }
     }
-    function aa(e){
-        alert(e.target)
-    }
+
+    // 事件委托
+    // function sEvtEntruse(e){
+    //     h = s = 1; 
+    //    var  e = e || window.e;
+    //     var eTget = e.target || e.srcElement;
+    //     for (; k<cateLkLen; k++){
+    //         aCateLk[k].idx = k;             
+    //     }
+        
+    //     if(eTget.nodeName == 'LI' || eTget.nodeName == 'A'){
+    //         addClass(aCateLs[eTget.idx], 'show');
+    //     }       
+    // }
+    // function HEvtEntruse(e){
+    //     var index = this.idx;        
+    //     h = s = 0; 
+    //     var  e = e || window.e;
+    //     var eTget = e.target || e.srcElement;
+    //     for (; k<cateLkLen; k++){
+    //         aCateLk[k].idx = k;             
+    //     }
+        
+    //     if(eTget.nodeName == 'LI' || eTget.nodeName == 'A'){
+    //         rmClass(aCateLs[eTget.idx], 'show');
+    //     }          
+    // }
 
     function show(){   
-        h = s = 1;                       
+             h = s = 1;                       
             addClass(oRegionalLs,'show');
+            addClass(oCurntPlace,'active-bg');
     }
     function hide(){
         h = s = 0;
         setTimeout(function(){
             if(h !==1 || s !== 1){
                 rmClass(oRegionalLs, 'show');
+                rmClass(oCurntPlace, 'active-bg');
             }
         },100);
     }
@@ -64,30 +95,33 @@
         //  var reg1 =  /\bJ_nav_lk\b/;
         //  var reg2 = /\bcate-item\b/;
         // 通过正则判断当前移入的是那个目标，让所有
-        //隐藏列表共用同一个显示隐藏函数,
-            addClass(aNavLs[index], 'show');             
+        // 隐藏列表共用一个显示隐藏函数,
+            addClass(aNavLs[index], 'show');  
+            addClass(aNavLk[index], 'active-bg');            
      }
      function hideNavList(){
         var index = this.idx;
          h = s = 0;       
         if(h !==1 || s !== 1){
             rmClass(aNavLs[index], 'show'); 
+            rmClass(aNavLk[index], 'active-bg'); 
         }                  
      }
 
      function showCateList(){
         var index = this.idx;
         h = s = 1; 
-           addClass(aCateLs[index], 'show');
+        addClass(aCateLs[index], 'show');
     }
     function hideCateList(){
         var index = this.idx;
-         h = s = 0;       
+        h = s = 0;       
         if(h !==1 || s !== 1){
-            // rmClass(aNavLs[index], 'show'); 
             rmClass(aCateLs[index], 'show'); 
         }                  
      } 
+
+     
     init();
 })(document);
 
